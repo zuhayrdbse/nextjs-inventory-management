cfe870935409e7a0eafd7b8a884a3dbf52d942d1
zuhayrdbse <zuhayrdanish2@gmail.com>
2024-09-14 01:21:23 +0530

    first commit

---
+++ server/prisma/migrations/20240911163852_init/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `date` to the `ExpenseByCategory` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Purchases" DROP CONSTRAINT "Purchases_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Sales" DROP CONSTRAINT "Sales_productId_fkey";
+
+-- AlterTable
+ALTER TABLE "ExpenseByCategory" ADD COLUMN     "date" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "PurchaseSummary" ALTER COLUMN "changePercentage" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Sales" ADD CONSTRAINT "Sales_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("productId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Purchases" ADD CONSTRAINT "Purchases_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("productId") ON DELETE CASCADE ON UPDATE CASCADE;
